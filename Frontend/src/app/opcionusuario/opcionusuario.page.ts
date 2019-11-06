d8b5a464ff7b22299e1b3c225f0cbfaeea3efddb
diego72 <diego_72dalmata@hotmail.com>
2019-11-05 23:12:51 -0500

    no message

---
+++ Frontend/src/app/opcionusuario/opcionusuario.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opcionusuario',
+  templateUrl: './opcionusuario.page.html',
+  styleUrls: ['./opcionusuario.page.scss'],
+})
+export class OpcionusuarioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
