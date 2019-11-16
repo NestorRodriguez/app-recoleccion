b3f5d282e3bc26cda7435f948579ec643d4d6d92
diego72 <diego_72dalmata@hotmail.com>
2019-11-16 07:20:36 -0500

    autenticacion de login

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard} from "./guards/auth.guard";
+import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'opcionusuario', loadChildren: './opcionusuario/opcionusuario.module#OpcionusuarioPageModule' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'opcionusuario', loadChildren: './opcionusuario/opcionusuario.module#OpcionusuarioPageModule', canActivate : [AuthGuard] },
   { path: 'chat', loadChildren: './chat/chat.module#ChatPageModule' },
   { path: 'mapas', loadChildren: './mapas/mapas.module#MapasPageModule' },
+
+
 
 ];
 
---
+++ Frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
+import { Observable, from } from 'rxjs';
+import { promise } from 'protractor';
+import {AngularFireAuth} from "@angular/fire/auth";
+import { auth } from 'firebase';
+import {map} from "rxjs/operators";
+import { isNullOrUndefined } from 'util';
+import{router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+constructor(private AFauth : AngularFireAuth,
+  private router: Router){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+return this.AFauth.authState.pipe(map(auth =>{
+  if(isNullOrUndefined(auth)){
+    this,router.navigate(['/home'])
+  return false;
+}else{
+return true
+}
+//console.log(auth);
+
+//return false;
+}))
+
+    }
+   
+
+}
